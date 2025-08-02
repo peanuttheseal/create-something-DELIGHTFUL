a99031f340026d1750376d36c2923fcefda1ab06
claire / seal <claire_lai_2028@mgs.sch.edu.sg>
2025-08-02 10:32:23 +0800

    Initial Commit

---
+++ create something DELIGHTFUL/create_something_DELIGHTFULApp.swift
@@ -0,0 +1,17 @@
+//
+//  create_something_DELIGHTFULApp.swift
+//  create something DELIGHTFUL
+//
+//  Created by T Krobot on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct create_something_DELIGHTFULApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
